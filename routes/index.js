5f1b718aa674e75db0e3453fc4b13ccd82f60d2f
Aymane Ait Laasri <aymaneaitlaasri@gmail.com>
2021-07-15 22:26:17 +0100

    cleaned the code a bit

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var userRepo = require('../repositories/user')
+var tagRepo = require('../repositories/tags')
+var projectRepo = require('../repositories/projects')
+var articleRepo = require('../repositories/articles')
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
+  let usersCount = await userRepo.countUsers()
+  let tagsCount = await tagRepo.countTags()
+  let articlesCount = await articleRepo.countArticles()
+  let projectsCount = await projectRepo.countProjects()
   let data = {
-    title: 'Admin Panel'
+    activePage: 'home',
+    title: 'Admin Panel',
+    usersNum: usersCount,
+    tagsNum: tagsCount,
+    articlesNum: articlesCount,
+    projectsNum: projectsCount
   }
 
   res.render('index.html', data)
 })
 router.get('/users', async function (req, res, next) {
+  let users = await userRepo.getAllUsers()
   let data = {
+    activePage: 'users',
     title: 'Users',
-    data_variable: require('../views/component/variables.json')
+    usersArray: users
   }
 
   res.render('pages/users.html', data)
 })
-
 module.exports = router;
